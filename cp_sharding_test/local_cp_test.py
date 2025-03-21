a07e54eab9e508af97129f9c1a35d052fd28b695
Eric Liu <eliu4913@usc.edu>
2025-03-21 18:01:59 +0000

    added simple per doc cp sharding ipynb

--- cp_sharding_test/local_cp_test.py
+++ cp_sharding_test/local_cp_test.py
@@ -7,3 +7,17 @@ from transformer_engine.pytorch.attention import (
 import torch
 import torch.distributed as dist
 import torch.multiprocessing as mp
+
+def run(rank, world_size):
+    dist.init_process_group(backend="nccl", rank=rank, world_size=world_size)
+    dist_group = dist.new_group(ranks=list(range(world_size)))
+    
+    # ================== #
+    # fuction to be tested
+    # ================== #
+
+    dist.destroy_process_group()
+
+if __name__ == "__main__":
+    world_size = 4
+    mp.spawn(run, args=(world_size,), nprocs=world_size)
